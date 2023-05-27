9296d2669ea0375bfebbfc069b701a429a357772
Habib Ur Rehman <habiburrehman2018@outlook.com>
2023-05-27 12:34:42 +0500

    Classes Construtor

--- index.js
+++ index.js
@@ -382,3 +382,55 @@ function main() {
     
     console.log(getCount(objects));
 }
+
+
+//  Classes 
+
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+
+// Method 1
+class Polygon {
+    constructor(arr) {
+      this.perimeter = function() {
+        let sum = 0;
+        arr.forEach((item) => {
+            sum += item;
+        });
+        return sum;
+    }
+    }
+
+    
+}
+
+// Method 2
+
+class Polygon {
+    constructor(arr) {
+        this.arr = arr;
+      this.perimeter = perimeter;
+    }
+
+}
+
+  function perimeter(arr) {
+        let sum = 0;
+      this.arr.forEach((item) => {
+            sum += item;
+        });
+        return sum;
+    }
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
